c2c2094596c6531ed7b24bce6729e5c2a7dcbf44
Ali Ghahari <a.ghahari@bimemahan.com>
2023-01-10 11:36:45 +0330

    added seeder

--- database/migrations/2023_01_08_170856_create_categories_table.php
+++ database/migrations/2023_01_08_170856_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->enum('flag', ['true', 'false'])->default('true'); // True For Building And False For Other Categories.
+            $table->boolean('flag')->default(0); // 1 For Building And 0 For Other Categories.
             $table->timestamps();
         });
     }
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            [
+                'name' => 'building', 
+                'flag' => 1, 
+                'created_at' => now(), 
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'shopping', 
+                'flag' => 0, 
+                'created_at' => now(), 
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'education', 
+                'flag' => 0,  
+                'created_at' => now(), 
+                'updated_at' => now()
+            ]
+        ];
+
+        Category::insert($categories);
+    }
+}
